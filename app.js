5d2743c9ef86dbbbc61ac31673c97ea49adcb473
alighous150 <97381381+alighous150@users.noreply.github.com>
2022-01-10 21:48:24 +0500

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ function getValue()
     document.getElementById('Vb').value='';
     document.getElementById('Vc').value='';
     if(arr[0]=='' || arr[1]=='' || arr[2]=='' || arr[3]=='' || arr[4]=='' || arr[5]=='' )
-    {alert("please provide phasors..")}
+    {alert("plz,provide phasors.......")}
    else{return arr }
    
 }
@@ -34,9 +34,24 @@ function faultAnalysis()
     }
     if(flag==false)
     {
+        var voltage=[];
+        var voltageAngle=[];
         var current=[];
         var angle=[];
-    //  for current Ia......................................
+    //  for current Ia and Va......................................
+        var Va=data[3];
+        var rva;
+        for(var i=0;i<Va.length;i++)
+       {
+           if(i==Va.indexOf("<"))
+           {
+               Va=Va.slice(0,i);
+               voltage.push(Number(Va));
+               rva=data[3].slice(i+1);
+               voltageAngle.push(Number(rva));
+
+           }
+       }
         var Ia=data[0];
         var ra;
        for(var i=0;i<Ia.length;i++)
@@ -51,6 +66,20 @@ function faultAnalysis()
            }
        }
         // for current Ib.......................................
+        var Vb=data[4];
+        var rvb;
+        for(var i=0;i<Vb.length;i++)
+       {
+           if(i==Vb.indexOf("<"))
+           {
+               Vb=Vb.slice(0,i);
+               voltage.push(Number(Vb));
+               rvb=data[4].slice(i+1);
+               voltageAngle.push(Number(rvb));
+
+           }
+       }
+        
         var Ib=data[1];
         var rb;
        for(var i=0;i<Ib.length;i++)
@@ -65,6 +94,20 @@ function faultAnalysis()
            }
        }
     //    for current Ic..........................................
+    var Vc=data[5];
+    var rvc;
+    for(var i=0;i<Vc.length;i++)
+   {
+       if(i==Vc.indexOf("<"))
+       {
+           Vc=Vc.slice(0,i);
+           voltage.push(Number(Vc));
+           rvc=data[5].slice(i+1);
+           voltageAngle.push(Number(rvc));
+
+       }
+   }    
+    
         var Ic=data[2];
         var rc;
        for(var i=0;i<Ic.length;i++)
@@ -78,7 +121,7 @@ function faultAnalysis()
 
            }
        }
-    Result(current,angle);
+    Result(current,angle,voltage,voltageAngle);
         
     }
     else
@@ -87,17 +130,23 @@ function faultAnalysis()
     }
     
 }
-function Result(current,angle){
+function Result(current,angle,voltage,voltageAngle){
 // polar to cartesian----------------------------------------
+ var cVa=math.Complex.fromPolar(+voltage[0],+voltageAngle[0]*Math.PI/180);
+ cVa=math.round(cVa,0);
+ var cVb=math.Complex.fromPolar(+voltage[1],+voltageAngle[1]*Math.PI/180);
+ cVb=math.round(cVb,0);
+ var cVc=math.Complex.fromPolar(+voltage[2],+voltageAngle[2]*Math.PI/180);
+ cVc=math.round(cVc,0);
  var cIa=math.Complex.fromPolar(+current[0],+angle[0]*Math.PI/180);
  cIa=math.round(cIa,0);
  var cIb=math.Complex.fromPolar(+current[1],+angle[1]*Math.PI/180);
  cIb=math.round(cIb,0);
  var cIc=math.Complex.fromPolar(+current[2],+angle[2]*Math.PI/180);
  cIc=math.round(cIc,0);
- console.log(cIa);
- console.log(cIb);
- console.log(cIc);
+//  console.log(cIa);
+//  console.log(cIb);
+//  console.log(cIc);
 //  -----------------------------------------------------------
     var a=math.Complex.fromPolar(1,120*Math.PI/180);
     var a_sq=math.Complex.fromPolar(1,-120*Math.PI/180);
@@ -111,18 +160,18 @@ function Result(current,angle){
      var I_z=math.add(cIa,cIb,cIc);
      I_z=math.divide(I_z,3);
      I_z=math.round(I_z,0);
-     console.log(I_z)
+    //  console.log(I_z)
     //  -------------------------------------------------------
      var I_p=math.add(cIa,aIb,a_sqI);
      I_p=math.divide(I_p,3);
      I_p=math.round(I_p,0);
-     console.log(I_p)
+    //  console.log(I_p)
     //---------------------------------------------------------
    
     var I_n=math.add(cIa,Ib_a_sq,Ic_a);
     I_n=math.divide(I_n,3);
     I_n=math.round(I_n,0);
-    console.log(I_n)
+    // console.log(I_n)
    // ---------------------------------------------------------
     if(I_z.equals(I_p) && I_p.equals(I_n) && I_n.equals(I_z))
     {
@@ -138,23 +187,14 @@ function Result(current,angle){
         alert("double line to ground fault")
 
     }
-   else if(I_n.equals(math.complex(0,0)) &&
-    I_z.equals(math.complex(0,0)) &&
-     Math.abs(angle[0]-angle[1])===120 && 
-     Math.abs(angle[1]-angle[2])===120 )
+   else if(I_z.equals(math.complex(0,0)) && math.add(cVa,cVb,cVc).equals(math.complex(0,0)))
     {
         alert("3-phase balanced fault")
     }
-    else if(current[0]===current[1] && 
-        current[1]===current[2]  &&  
-        Math.abs(angle[0]-angle[1])===120 && 
-        Math.abs(angle[1]-angle[2])===120 )
-    {
-        alert("normal conditions")
-    }
-
     else{
-        alert("Enter correct phasors")
+        alert("enter correct phasors")
     }
+    console.log(math.add(cVa,cVb,cVc))
+    
     
 }
